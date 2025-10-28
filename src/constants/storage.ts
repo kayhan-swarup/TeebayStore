3974d52485ec7cf6dd1a3166bb6fe871a47116c3
kayhan <kayhan9204@gmail.com>
2025-10-28 18:41:43 +0600

    async storage constants added

---
+++ src/constants/storage.ts
@@ -0,0 +1,17 @@
+export const STORAGE_KEYS = {
+  // Authentication
+  USER_TOKEN: '@teebay_user_token',
+  USER_DATA: '@teebay_user_data',
+  USER_ID: '@teebay_user_id',
+
+  // Biometric
+  BIOMETRIC_ENABLED: '@teebay_biometric_enabled',
+  BIOMETRIC_USERNAME: '@teebay_biometric_username',
+
+  // Firebase
+  FCM_TOKEN: '@teebay_fcm_token',
+
+  // App state
+  FIRST_LAUNCH: '@teebay_first_launch',
+  LAST_SYNC: '@teebay_last_sync',
+} as const;
