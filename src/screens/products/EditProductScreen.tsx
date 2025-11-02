802d97da5d6a886debdba693768318c0cdb923fe
kayhan <kayhan9204@gmail.com>
2025-11-02 16:43:36 +0600

    Initializes EditProductScreen

--- src/screens/products/EditProductScreen.tsx
+++ src/screens/products/EditProductScreen.tsx
@@ -1,7 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { RootStackParamList } from '../../types';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useProductStore } from '../../store/productStore';
+
+type EditProductRouteProp = RouteProp<RootStackParamList, 'EditProduct'>;
+type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
 
 const EditProductScreen = () => {
+  const route = useRoute<EditProductRouteProp>();
+  const navigation = useNavigation<NavigationProp>();
+  const { selectedProduct, isLoading, fetchProductById } = useProductStore();
   return (
     <View>
       <Text>EditProductScreen</Text>
